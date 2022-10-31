61fa0936b5df88b374eadf149e918cf7e90c85c6
shuu-wasseo <joshuasoh2008@gmail.com>
2022-10-31 23:00:45 +0800

    removed whitespace

--- main.py
+++ main.py
@@ -146,7 +146,7 @@ def daymap(begin, end, col, json):
         date = (start.year, start.month, start.day)
         nums = []
         for habit in lis:
-            print(date, habit)
+            print(start, date, end, habit)
             string = ""
             if habit != "stepno":
                 if habit == "overall":
