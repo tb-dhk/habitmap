ed35f7a5505f5db116805e28edb4fb56783f6dbb
shuu-wasseo <joshuasoh2008@gmail.com>
2022-10-31 15:01:35 +0800

    fixed

--- main.py
+++ main.py
@@ -191,6 +191,8 @@ def daymap(begin, end, col):
                 string += " "
                 while start <= end:
                     date = (start.year, start.month, start.day)
+                    if date[0] not in dic:
+                        newyear(json, habit, date[0])
                     num = dic[habit][str(date[0])][date[1]-1][date[2]-1]
                     nums.append(num)
                     match num:
