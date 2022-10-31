091d4a07d2acb8520ac09e36777f3c84298b5ab0
shuu-wasseo <joshuasoh2008@gmail.com>
2022-10-31 17:55:43 +0800

    colors

--- main.py
+++ main.py
@@ -210,8 +210,8 @@ def daymap(begin, end, col, json):
                 print(string)
 
 def monthmap(begin, end, col, json):
-    begin = dt.datetime.isoformat(dt.date(int(begin[0:4]), int(begin[5:7]), 1))
-    end = dt.datetime.isoformat(dt.date(int(begin[0:4]), int(begin[5:7]), calendar.monthrange(int(begin[0:4]), int(begin[5:7]))[1]))
+    begin = dt.datetime.isoformat(dt.datetime.combine(dt.date(int(begin[0:4]), int(begin[5:7]), 1)), datetime.min.time())
+    end = dt.datetime.isoformat(dt.datetime.combine(dt.date(int(begin[0:4]), int(begin[5:7]), calendar.monthrange(int(begin[0:4]), int(begin[5:7]))[1])), datetime.min.time())
     daymap(begin, end, col, json)
 
 def yearmap(habit, year, col):
