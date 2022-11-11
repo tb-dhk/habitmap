f2f243556988e4f1dc48154a7b821be702e12f1c
shuu-wasseo <joshuasoh2008@gmail.com>
2022-11-12 03:01:56 +0800

    .

--- acc.py
+++ acc.py
@@ -50,6 +50,7 @@ def gensalt(uid, pwd):
 def sync(user, dic, edic, con, econ):
     print("syncing database...")
     edic = json.dumps(edic).replace(r"\\", "").strip(r"'\\").strip('"')
+    print([econ])
     econ = toml.dumps(econ).replace(r"\\", "").strip(r"'\\").strip('"')
     if (edic in [{}, "null", None] and econ in [{}, "null", None]) and (dic in [{}, "null", None] and con in [{}, "null", None]):
         print("you have no data to import or export. happy habit tracking!")
